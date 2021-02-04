5292666287d289869fa92b7963e8a0e20de5bb68
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-04 01:19:00 +0100

    RWD hashtag plugin is getting more robust

--- site/plugins/hashtags/index.php
+++ site/plugins/hashtags/index.php
@@ -4,19 +4,16 @@ Kirby::plugin('rwd/hashtags', [
   'tags' => [
     'hashtag' => [
       'attr' => [
-        'alt',
-        'caption',
         'class',
-        'height',
-        'imgclass',
-        'link',
+        'text',
       ],
       'html' => function($tag) {      
         $hashtag  = $tag->value();
         $site = site();
         $lang = $site->language()->code();
+        $text = $tag->text ?? $hashtag;
         $url = url($site->url(), ['params' => ['tag' => urlencode($hashtag)]]);
-        return '<a class="taglink" href="' . $url  . '">#' . $hashtag  . '</a>';
+        return '<a class="taglink" href="' . $url  . '">#' . $text  . '</a>';
       }
     ]
   ]    
