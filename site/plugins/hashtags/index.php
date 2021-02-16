670194f4110154802f7b45a7c5f3e424d1f73cdc
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-16 17:30:13 +0100

    RWD loves marginalia, comments, clickable hashtags and nicely typeset quotes

--- site/plugins/hashtags/index.php
+++ site/plugins/hashtags/index.php
@@ -15,6 +15,22 @@ Kirby::plugin('rwd/hashtags', [
         $url = url($site->url(), ['params' => ['tag' => urlencode($hashtag)]]);
         return '<a class="taglink" href="' . $url  . '">#' . $text  . '</a>';
       }
+    ],
+
+    'comment' => [
+      'attr' => [
+        'class',
+        'author',
+      ],
+      'html' => function($tag) {  
+        $html = "<p class='comment'>";
+        $comment  = $tag->value();
+        if ($tag->author != '') {
+          $html .= '<span class="author">' . $tag->author . ': </span>';          
+        }
+        $html .= kirbytextinline($comment) . '</p>';
+        return $html;
+      }
     ]
   ]    
 ]);
