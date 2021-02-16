16b06a12c5aae9edecd9f297c988e7cd76c50056
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-16 18:05:17 +0100

    Problem with quotes kirbytag: trying to solve

---
+++ site/plugins/kirbytag-quote/index.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * kirby 3 kirbytag - quote
+ *
+ * Forked from https://github.com/jbeyerstedt/kirby-kirbytag-quote
+ * license: http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
+ */
+
+Kirby::plugin('accentgrave/quote', [
+  'options' => [
+    'default_style' => 'none'
+  ],
+  'tags' => [
+    'quote' => [
+      'attr' => [
+        'author',
+        'class',
+        'url',
+        'urltext'
+      ],
+      'html' => function($tag) {
+        $html = '';
+
+        $has_url = $tag->url != '';
+        $url = trim($tag->url);
+
+        $class = $tag->class;
+        $class .= ' ' . option('accentgrave.quote.blockquote_classname', '');        
+        $html .= '<blockquote class="' . $class . '" ' . e($has_url, 'cite="' . $url .'"'). '>';
+
+        $html .= '<p>' . kirbytextinline($tag->value) . '</p>';
+
+        if ($tag->author != '') {
+          
+          $link = r($has_url, ', <a href="'. $url .'">' . r($tag->urltext != "", $tag->urltext, "source") . '</a>', '');           
+          $html .= '<footer><span class="author">' . $tag->author . '</span>' . $link . '</footer>';          
+        }
+
+        $html .= '</blockquote>';
+        return $html;
+      }
+    ]
+  ]
+]);
