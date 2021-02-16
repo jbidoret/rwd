5ff79a336aab8acb64f706eccb3a811661abf23d
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-16 17:46:15 +0100

    Config for comments, foontnotes & markdown

--- site/config/config.php
+++ site/config/config.php
@@ -14,8 +14,38 @@ return [
 	'languages' => true,
 	'languages.detect' => true,
 
-	// Cachebuster plugin
-	'schnti.cachebuster.active' => false,
+	// Blockqotes
+	'accentgrave.quote.blockquote_classname' => "pullquote",
+
+	// MD extra
+	'markdown' => [
+    'extra' => true
+  ],
+
+	// Komments
+	// 'mauricerenck.komments.debug' => true,
+	// 'mauricerenck.komments.enable-webmention-support' => false,
+	// 'mauricerenck.komments.send-mention-on-update' => false,
+
+	// Commentions
+	'sgkirby.commentions.templatesWithComments' => ['note', 'link', 'interview', 'theme'],
+	'sgkirby.commentions.templatesWithWebmentions' => [],
+	'sgkirby.commentions.t.fr.snippet.list.dateFormat.date'     => 'd/m/y H:i',
+	'sgkirby.commentions.t.fr.snippet.list.dateFormat.strftime' => '%d/%m/%Y %H:%M',
+	'sgkirby.commentions.t.fr.snippet.form.email.optional'      => 'E-mail <span>optionel ; n’est pas affiché</span>',
+	'sgkirby.commentions.t.fr.snippet.form.website.optional'    => 'Site web <span>optionel ; public si saisi</span>',
+	'sgkirby.commentions.t.fr.snippet.form.ctacomment'          => 'Contribuez !',
+	'sgkirby.commentions.t.fr.snippet.form.comment'             => 'Messsage',
+	'sgkirby.commentions.t.fr.snippet.list.comments'						=> 'Contributions',
+	'sgkirby.commentions.t.fr.feedback.comment.queued'          => 'Merci ! Soyez patient·e, votre commentaire doit être validé avant publication : )',
+	'sgkirby.commentions.t.fr.feedback.comment.thankyou'        => 'Merci de votre commentaire!',
+	'sgkirby.commentions.t.fr.feedback.webmention.queued'       => 'Merci, votre webmention est en attente de traitement. Soyez patient·e, votre commentaire doit être validé avant publication : )',
+	'sgkirby.commentions.commentfields' => [
+		'name' => true,  // include name field and mark as required
+		'email',         // include email as optional field
+		'website',       // include optional website field
+	],
+	'sgkirby.commentions.hideforms' => true,
 
 	// Markdown plugin
 	'community.markdown-field.buttons'    => [['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], 'bold', 'italic', 'divider', 'link','pagelink', 'email', 'file', 'divider', 'ul', 'ol', 'blockquote'],
