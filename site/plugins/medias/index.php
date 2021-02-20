27ca8578dd4fc04ee899cb6586948c55c20405af
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-20 03:40:56 +0100

    Media plugin + initials method

---
+++ site/plugins/medias/index.php
@@ -0,0 +1,38 @@
+<?php
+
+Kirby::plugin('rwd/medias', [
+  'tags' => [
+    'media' => [
+      'attr' => [
+        'alt',
+        'caption',
+        'class',
+      ],
+      'html' => function ($tag) {
+        if ($tag->file = $tag->file($tag->value)) {
+          $tag->media_type = $tag->file->media_type()->value();
+          $tag->src = $tag->media_type == 'youtube' ? $tag->file->yt_url() : $tag->file->url();
+
+          if ($tag->file->media_type() == 'youtube') {
+            $media_label = 'Vidéo';
+          } elseif  ($tag->file->media_type() == 'image') {
+            $media_label = 'Image';
+          } else {
+            $media_label = 'WTF';
+          }
+          
+          $tag->alt = $tag->alt ?? $tag->file->alt()->or(' ')->value();
+          $tag->caption = $tag->caption ?? $tag->file->caption()->value();
+          $tag->source = $tag->source ?? $tag->file->source()->value();
+          
+          $html = '<a href="' . $tag->src . '" class="media-link" data-type="' . $tag->media_type . '" data-alt="' . $tag->alt . '"  data-source="' . $tag->source . '">' . $media_label . '</a>';
+          $html .= '<span class="media-caption">' . kirbytextinline($tag->caption) . '</span>';
+          
+
+          return $html;
+            
+        }
+      }
+    ]
+  ]
+]);
--- site/plugins/rwd/index.php
+++ site/plugins/rwd/index.php
@@ -8,6 +8,26 @@ Kirby::plugin('rwd/helpers', [
             return $str;            
         }
     ],
+
+    'fieldMethods' => [
+        'initials' => function($field, $name){
+            $words = explode(' ', $name);
+            if (count($words) >= 2) {
+                $initials = strtoupper(substr($words[0], 0, 1) . substr(end($words), 0, 1));
+            } else {
+              preg_match_all('#([A-Z]+)#', $name, $capitals);
+              if (count($capitals[1]) >= 2) {
+                $initials = substr(implode('', $capitals[1]), 0, 2);
+              } else {
+                $initials = strtoupper(substr($name, 0, 2));
+              }
+            }
+            $initials_html = '<span class="initials" title="' . $name . '">' . $initials . ' – </span>'; 
+            $first_p_pos = strpos( $field, 'p>' );
+            $new_text = substr_replace( $field, $initials_html, $first_p_pos + 2, 0 );
+            return $new_text;
+        }
+    ],
     
     'templates' => [
       'notes'   => kirby()->root('templates') . '/list.php',
