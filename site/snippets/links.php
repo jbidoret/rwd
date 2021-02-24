7cd6e0924199c62e61beb6c148abaffcef6ad5ab
Julien Bidoret – accentgrave <julien@accentgrave.net>
2021-02-24 14:55:58 +0100

    Nav links include link to tweets

--- site/snippets/links.php
+++ site/snippets/links.php
@@ -10,6 +10,7 @@
     <li><a href="<?=page('notes')->url()?>"><?= t("Notes") ?></a></li>
     <li><a href="<?=page('links')->url()?>"><?= t("Links") ?></a></li>
     <li><a href="<?=page('interviews')->url()?>"><?= t("Interviews") ?></a></li>
+    <li><a href="<?=page('tweets')->url()?>"><?= t("Tweets") ?></a></li>
     <li><a href="<?=page('about')->url()?>"><?= t("About") ?></a></li>
   </ul>
 </div>
